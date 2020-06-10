a174c03e045e76a0294d93f252efb55c4618356a
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 11:58:00 +0200

    correction

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>partie 3 exercice 4</title>
 </head>
 <body>
-<?php
+    <?php
         $nb1 = 1;
-        while ($nb1 <= 10) {
-            echo '<p>' . $nb1 . '</p>';
-            $nb1 = $nb1 + ($nb1 / 2);
-        }
-    ?>
+        while ($nb1 < 10) {?>
+            <p><?php echo $nb1 ?></p>
+            <?php $nb1 = $nb1 + ($nb1 / 2);
+        }?>
 </body>
 </html>
